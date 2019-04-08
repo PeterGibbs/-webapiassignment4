7edd57b28dfcdcd26c805245caf3935902d7bc13
Peter Gibbs <pgibbs65@gmail.com>
2019-04-07 21:50:07 -0600

    I am debugging in production

--- server.js
+++ server.js
@@ -53,12 +53,10 @@ app.post('/signup',function(req,res){
             }else{
                 console.log("adding")
                 const hash_secret=process.env.HASH_SECRET;
-                var hash =new crypto.createHmac('sha256', hash_secret)
                 var newUser=User({
                     username: req.body.username,
-                        password: hash.update(req.body.password).digest('hex')
+                        password: crypto.createHmac('sha256', hash_secret).update(req.body.password).digest('hex')
                     });
-                    
                     newUser.save(function(err){
                         if(err) throw err;
                         let responseData={
@@ -93,8 +91,7 @@ app.all('/signup',function(req,res){
 app.post('/signin',function(req,res){
     console.log(req.body);
     const hash_secret=process.env.HASH_SECRET;
-    var hash =new crypto.createHmac('sha256', hash_secret)
-    var pwd=hash.update(req.body.password).digest('hex')
+    var pwd=crypto.createHmac('sha256', hash_secret).update(req.body.password).digest('hex')
     User.findOne({username:req.body.username,password:pwd},function(err,user){
         if(err) throw err;
         if(!user){
@@ -249,7 +246,7 @@ app.route('/movies').put(authJwtController.isAuthenticated,function(req,res){
 });
 app.route('/movies').delete(authJwtController.isAuthenticated,function(req,res){
     if(req.body.title){
-        Review.findAllAndRemove({MovieTitle:req.body.title},function(err,review){
+        Reviews.findAllAndRemove({MovieTitle:req.body.title},function(err,review){
             
             if (err) throw err;
         
